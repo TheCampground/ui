dda8271e9fdffe13a3e81caa372fbef8cb62b1ab
Brad <44570552+bizojs@users.noreply.github.com>
2025-10-29 15:05:37 +0000

    fix: return to top of page on navigate
    The current behaviour prevented scrolling on navigation due to the body/html having `overflow: hidden`.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { afterNavigate } from "$app/navigation"
 	import "../app.css"
     import "@fontsource-variable/space-grotesk"
 
 	let { children } = $props()
+
+   	afterNavigate((nav) => {
+        document.querySelectorAll("main").forEach((el) => {
+            el.scrollTo({ top: 0 })
+        })
+   	})
 </script>
 
 {@render children?.()}
