ed0970d0fcd02be5128d5298fa2890c64cb47337
Brad <44570552+bizojs@users.noreply.github.com>
2025-10-31 19:32:50 +0000

    refactor(card): image mask

--- src/lib/components/card/card.svelte
+++ src/lib/components/card/card.svelte
@@ -15,7 +15,7 @@
     "rounded-card-lg bg-background shadow-popover dark:shadow-none gap-4 border overflow-hidden"
 )}>
     {#if media}
-        <img src={media} class="min-h-30 -mt-10 b-20 w-full h-fit mask-[linear-gradient(to_bottom,black_50%,transparent_100%)] [-webkit-mask-image:linear-gradient(to_bottom,black_50%,transparent_100%)] pointer-events-none select-none" alt="" draggable="false" />
+        <img src={media} class="min-h-30 -mt-10 b-20 w-full h-fit mask-[linear-gradient(to_bottom,black_50%,transparent_95%)] [-webkit-mask-image:linear-gradient(to_bottom,black_50%,transparent_95%)] pointer-events-none select-none" alt="" draggable="false" />
     {/if}
     <span class={cn(className, media ? "-mt-20 p-7" : "p-0", "w-full")}>
         {@render children?.()}
