dd2482b424899f9c21144fb72ac27072480aeba4
Brad <44570552+bizojs@users.noreply.github.com>
2025-10-31 19:32:25 +0000

    refactor(examples): use Campground icon

--- src/lib/internal/examples/button-sizes.svelte
+++ src/lib/internal/examples/button-sizes.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { Button } from "$lib/index.ts"
+
+    import { Campground } from "@internal/icons/index.ts"
+    import { Icon } from "@steeze-ui/svelte-icon"
 </script>
 
 <div class="flex gap-10 flex-wrap">
@@ -12,7 +15,7 @@
     <div class="flex flex-col gap-2">
         <p class="text-foreground-alt text-sm">Icon</p>
         <Button variant="primary" size="icon">
-            <img src="/favicon-dark.png" class="size-5 select-none pointer-events-none" alt="" draggable="false" />
+            <Icon src={Campground} class="size-5 text-brand-foreground" />
         </Button>
     </div>
     <div class="flex flex-col gap-2">
