2c331d398003c2faa5a3ddc769aa0676e91a62b8
Brad <44570552+bizojs@users.noreply.github.com>
2025-11-02 11:39:44 +0000

    refactor(props): only show table if items exist

--- src/lib/internal/props/props.svelte
+++ src/lib/internal/props/props.svelte
@@ -16,68 +16,70 @@
 </script>
 
 <div class="flex flex-col gap-4">
-    <div class="flex flex-col gap-1">
+    <div class={cn("flex flex-col gap-1", items.length < 1 && "mb-5")}>
         {@render componentName(component)}
-        <p class="text-sm text-foreground-alt">{description}</p>
+        <p class="text-sm text-foreground-alt">{@html description}</p>
     </div>
-    <table>
-        <thead>
-            <tr>
-                <th class="text-start w-50 px-2 py-2">Property</th>
-                <th class="text-start w-60 px-2 py-2 hidden lg:table-cell">Type</th>
-                <th class="text-start px-2 py-2 hidden lg:table-cell">Description</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each items as item (item.property)}
-                <tr class="border-t">
-                    <td class="align-top text-start w-full lg:w-50 p-2 flex justify-between lg:justify-start items-center gap-1" id="property">
-                        <div class="flex items-center gap-1">
-                            {@render property(item)}
-                            {@render bindable(item)}
-                            {@render required(item)}
-                        </div>
-                        <span class="block lg:hidden">
-                            <Popover useOverlay buttonText={Info} buttonSize="icon" buttonVariant="ghost" buttonClass="!size-6 align-center">
-                                <div class="flex flex-col gap-4 w-full">
-                                    <div class="flex w-full items-center justify-between gap-2">
-                                        <p class="text-sm font-semibold">{item.property}</p>
-                                        {@render bindable(item, false)}
-                                        {@render required(item, false)}
-                                    </div>
-                                    <Separator class="my-0!" />
-                                    <code>{item.type}</code>
-                                    {#if item?.typeDef}
-                                        <span class="max-w-[calc(100vw-5rem)] whitespace-break-spaces">
-                                        {@html ShikiHandler.codeToHtml(item.typeDef, "typescript")}
-                                        </span>
-                                    {/if}
-                                    <Separator class="my-0!" />
-                                    <div class="flex flex-col w-full">
-                                        <p class="text-sm font-medium">Description</p>
-                                        <p class="text-xs text-foreground-alt max-w-[calc(100vw-5rem)] whitespace-break-spaces">
-                                            {@html item.description}
-                                        </p>
-                                    </div>
-                                </div>
-                            </Popover>
-                        </span>
-                    </td>
-                    <td class="align-top text-start w-60 p-2 text-sm font-medium font-mono hidden lg:table-cell" id="type">
-                        {item.type}
-                        {@render callback(item)}
-                    </td>
-                    <td class="align-top text-start p-2 hidden lg:flex flex-col" id="description">
-                        <p class="text-sm">{@html item.description}</p>
-                        <p class="text-sm text-foreground-alt font-medium font-code">
-                            Default:
-                            {@render defaultProp(item)}
-                        </p>
-                    </td>
+    {#if items.length > 0}
+        <table>
+            <thead>
+                <tr>
+                    <th class="text-start w-50 px-2 py-2">Property</th>
+                    <th class="text-start w-60 px-2 py-2 hidden lg:table-cell">Type</th>
+                    <th class="text-start px-2 py-2 hidden lg:table-cell">Description</th>
                 </tr>
-            {/each}
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {#each items as item (item.property)}
+                    <tr class="border-t">
+                        <td class="align-top text-start w-full lg:w-50 p-2 flex justify-between lg:justify-start items-center gap-1" id="property">
+                            <div class="flex items-center gap-1">
+                                {@render property(item)}
+                                {@render bindable(item)}
+                                {@render required(item)}
+                            </div>
+                            <span class="block lg:hidden">
+                                <Popover useOverlay buttonText={Info} buttonSize="icon" buttonVariant="ghost" buttonClass="!size-6 align-center">
+                                    <div class="flex flex-col gap-4 w-full">
+                                        <div class="flex w-full items-center justify-between gap-2">
+                                            <p class="text-sm font-semibold">{item.property}</p>
+                                            {@render bindable(item, false)}
+                                            {@render required(item, false)}
+                                        </div>
+                                        <Separator class="my-0!" />
+                                        <code>{item.type}</code>
+                                        {#if item?.typeDef}
+                                            <span class="max-w-[calc(100vw-5rem)] whitespace-break-spaces">
+                                            {@html ShikiHandler.codeToHtml(item.typeDef, "typescript")}
+                                            </span>
+                                        {/if}
+                                        <Separator class="my-0!" />
+                                        <div class="flex flex-col w-full">
+                                            <p class="text-sm font-medium">Description</p>
+                                            <p class="text-xs text-foreground-alt max-w-[calc(100vw-5rem)] whitespace-break-spaces">
+                                                {@html item.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </Popover>
+                            </span>
+                        </td>
+                        <td class="align-top text-start w-60 p-2 text-sm font-medium font-mono hidden lg:table-cell" id="type">
+                            {item.type}
+                            {@render callback(item)}
+                        </td>
+                        <td class="align-top text-start p-2 hidden lg:flex flex-col" id="description">
+                            <p class="text-sm">{@html item.description}</p>
+                            <p class="text-sm text-foreground-alt font-medium font-code">
+                                Default:
+                                {@render defaultProp(item)}
+                            </p>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {/if}
 </div>
 
 
