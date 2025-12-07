cfbaef21dc2593230cfb1779c2506d7befba1369
brys0 <brystn@proton.me>
2025-12-07 10:32:36 -0600

    fix: font loading for @fontsource deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-    import { afterNavigate } from "$app/navigation"
-    import { Noise, Toaster } from "$lib/index.ts"
-    import { ModeWatcher } from "mode-watcher"
+	import { afterNavigate } from "$app/navigation"
+	import { Noise, Toaster } from "$lib/index.ts"
+	import { ModeWatcher } from "mode-watcher"
 
 	import "../app.css"
-    import "@fontsource-variable/space-grotesk"
+	import "@fontsource-variable/space-grotesk"
 
 	let { children } = $props()
 
-   	afterNavigate(() => {
-        document.querySelectorAll("main").forEach((el) => {
-            el.scrollTo({ top: 0 })
-        })
-   	})
+	afterNavigate(() => {
+		document.querySelectorAll("main").forEach((el) => {
+			el.scrollTo({ top: 0 })
+		})
+	})
 </script>
 
 <Toaster />
